d51c80f4174d31bb876ac992def49dc4692f042d
Konstantin Sulima <konstantin.sulima@sulimak.com>
2021-07-28 23:07:00 +0300

    basic refactoring and restructuring

--- main.go
+++ main.go
@@ -11,10 +11,17 @@ import (
 
 var timerCount = 5
 var correct = 0
+var isRandomized bool
+var timeFlag *int
+
+type problem struct {
+	question string
+	answer string
+}
 
 func main() {
 	csvFileName := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
-	timeFlag := flag.Int("timer", timerCount, "timer for answering to a question")
+	timeFlag = flag.Int("timer", timerCount, "timer for answering to a question")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -28,54 +35,53 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided csv file.")
 	}
-
 	problems := parseLines(lines)
 
 	fmt.Println("Please press Enter key to start the quiz:")
-
 	fmt.Scanln()
-	for i, p := range problems {
-		timer := time.NewTimer(time.Duration(*timeFlag) * time.Second)
-		go func() {
-			select {
-			case <-timer.C:
-				printResults(len(problems))
-				exit("Time expired.\n")
-			}
-		}()
-		if checkAnswers(i, p.q, p.a) {
-			correct++
-		}
-		timer.Stop()
-	}
+
+	processAnswers(problems)
+
 	printResults(len(problems))
-}
-
-func printResults(problems int) {
-	fmt.Printf("Final score %d out of %d.", correct, problems)
-}
-
-func checkAnswers(i int, q, a string) (c bool) {
-	fmt.Printf("Problem %d: %s = \n", i+1, q)
-	var answer string
-	fmt.Scanf("%s", &answer)
-	return a == answer
 }
 
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
 		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
+			question: line[0],
+			answer: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
 }
 
-type problem struct {
-	q string
-	a string
+func processAnswers(problems []problem) {
+	for i, problem := range problems {
+		timer := time.NewTimer(time.Duration(*timeFlag) * time.Second)
+		go func() {
+			select {
+			case <-timer.C:
+				printResults(len(problems))
+				exit("Time expired.")
+			}
+		}()
+		if checkAnswers(i+1, problem.question, problem.answer) {
+			correct++
+		}
+		timer.Stop()
+	}
+}
+
+func checkAnswers(i int, question, answer string) bool {
+	fmt.Printf("Problem %d: %s = \n", i, question)
+	var userAnswer string
+	fmt.Scanf("%s", &userAnswer)
+	return answer == userAnswer
+}
+
+func printResults(problems int) {
+	fmt.Printf("Final score %d out of %d.\n", correct, problems)
 }
 
 func exit(msg string) {
